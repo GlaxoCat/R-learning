f26e0aa8dc130fdb16d0b62a1cb4749e5e0beb90
GlaxoCat <37057876+GlaxoCat@users.noreply.github.com>
2026-07-08 16:31:10 -0400

    added boxplot and violin plot

--- 03-data-transformation/Imaris_Plastics_UpScale.R
+++ 03-data-transformation/Imaris_Plastics_UpScale.R
@@ -6,29 +6,40 @@ library(readxl)
 library(here)
 
 process_image_data <- function(image_prefix) {
-  
-  # here() tells R: "Start at the root project directory, 
+  # here() tells R: "Start at the root project directory,
   # look inside 'Imaris_Data', and find this file."
   cluster_center <- read_excel(
-    here("Imaris_Data", paste0(image_prefix, "_Cluster_Plastics_GFP_CenterIntensity.xlsx")), 
+    here(
+      "Imaris_Data",
+      paste0(image_prefix, "_Cluster_Plastics_GFP_CenterIntensity.xlsx")
+    ),
     skip = 1
   ) %>%
     select(ID, Center_Intensity = `Intensity Center`)
 
   cluster_max <- read_excel(
-    here("Imaris_Data", paste0(image_prefix, "_Cluster_Plastics_GFP_MaxIntensity.xlsx")), 
+    here(
+      "Imaris_Data",
+      paste0(image_prefix, "_Cluster_Plastics_GFP_MaxIntensity.xlsx")
+    ),
     skip = 1
   ) %>%
     select(ID, Max_Intensity = `Intensity Max`)
 
   cluster_mean <- read_excel(
-    here("Imaris_Data", paste0(image_prefix, "_Cluster_Plastics_GFP_MeanIntensity.xlsx")), 
+    here(
+      "Imaris_Data",
+      paste0(image_prefix, "_Cluster_Plastics_GFP_MeanIntensity.xlsx")
+    ),
     skip = 1
   ) %>%
     select(ID, Mean_Intensity = `Intensity Mean`)
 
   cluster_voxel <- read_excel(
-    here("Imaris_Data", paste0(image_prefix, "_Cluster_Plastics_GFP_Voxel.xlsx")), 
+    here(
+      "Imaris_Data",
+      paste0(image_prefix, "_Cluster_Plastics_GFP_Voxel.xlsx")
+    ),
     skip = 1
   ) %>%
     select(ID, Voxel = `Number of Voxels`)
@@ -44,25 +55,37 @@ process_image_data <- function(image_prefix) {
 
   # --- Same Process for Non-cluster plastics ---
   noncluster_center <- read_excel(
-    here("Imaris_Data", paste0(image_prefix, "_NonCluster_Plastics_GFP_CenterIntensity.xlsx")), 
+    here(
+      "Imaris_Data",
+      paste0(image_prefix, "_NonCluster_Plastics_GFP_CenterIntensity.xlsx")
+    ),
     skip = 1
   ) %>%
     select(ID, Center_Intensity = `Intensity Center`)
-    
+
   noncluster_max <- read_excel(
-    here("Imaris_Data", paste0(image_prefix, "_NonCluster_Plastics_GFP_MaxIntensity.xlsx")), 
+    here(
+      "Imaris_Data",
+      paste0(image_prefix, "_NonCluster_Plastics_GFP_MaxIntensity.xlsx")
+    ),
     skip = 1
   ) %>%
     select(ID, Max_Intensity = `Intensity Max`)
 
   noncluster_mean <- read_excel(
-    here("Imaris_Data", paste0(image_prefix, "_NonCluster_Plastics_GFP_MeanIntensity.xlsx")), 
+    here(
+      "Imaris_Data",
+      paste0(image_prefix, "_NonCluster_Plastics_GFP_MeanIntensity.xlsx")
+    ),
     skip = 1
   ) %>%
     select(ID, Mean_Intensity = `Intensity Mean`)
 
   noncluster_voxel <- read_excel(
-    here("Imaris_Data", paste0(image_prefix, "_NonCluster_Plastics_GFP_Voxel.xlsx")), 
+    here(
+      "Imaris_Data",
+      paste0(image_prefix, "_NonCluster_Plastics_GFP_Voxel.xlsx")
+    ),
     skip = 1
   ) %>%
     select(ID, Voxel = `Number of Voxels`)
@@ -81,7 +104,7 @@ process_image_data <- function(image_prefix) {
 
 # Read only the ID and the raw metric from each independent file
 # List all your image prefixes
-image_list <- c("image14", "image28") 
+image_list <- c("image1", "image14", "image28")
 
 # Read and combine everything automatically
 final_table <- map_df(image_list, process_image_data)
@@ -163,3 +186,26 @@ ggplot(
   theme_prism(base_size = 14) +
   scale_fill_prism(palette = "prism_light") +
   scale_color_prism(palette = "prism_light")
+
+# # ==========================================
+# # Comparing Mean Intensities of plastics across different images, grouped by cluster and noncluster
+# # ==========================================
+ggplot(
+  final_table,
+  aes(x = Mean_Intensity, fill = Image_ID)
+) +
+  geom_density(alpha = 0.5) +
+  facet_wrap(~Spot_Category) +
+  labs(
+    title = "Mean Intensity of Spots across images",
+    x = "Image",
+    y = "Freq",
+    color = "Image ID"
+  ) +
+  theme_prism(base_size = 14) +
+  scale_fill_prism(palette = "prism_light") +
+  scale_color_prism(palette = "prism_light")
+
+# # ==========================================
+# # This comment is a test for commit and ammend
+# # ==========================================
